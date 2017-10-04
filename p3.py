dea086403dea816d35a53424fa6086f7cd28bbdc
Charles Laing <cclaing@ualberta.ca>
2017-10-04 10:20:36 -0600

    Finished p2 added p3

--- p3.py
+++ p3.py
@@ -2,7 +2,12 @@ import sys
 '''
 Student: Charles Laing ID: 1388069
 
-Greated an adjacency list to store a graph data struct
+creates an adjacency list to store a graph data struct
+outputs the connected components of the graph in the format 
+
+[num components]
+[num elements][element list]
+..
 
 '''
 
@@ -26,23 +31,26 @@ class Graph:
         self.nodeList[dest].head.append(source)
 
     def connected(self):
-        visited =[]
-        [visited.append(False) for i in range(self.v)]
-        components = []
-        for i in range(self.v):
-            if (visited[i] == False):
-                tempvar = self.DFSutl(i, visited)
-                if (len(tempvar)):                    
+        visited =[] #Create an open list to contain all unvisited nodes
+        [visited.append(False) for i in range(self.v)] #All of them are false, so fill it with a list comp
+        components = [] # place to put the strings
+        for i in range(self.v): #for each node in the list
+            if (visited[i] == False): #check if we haven't seen it yet
+                tempvar = self.DFSutl(i, visited) #Call the util to iterate through the second structure
+                if (len(tempvar)): #Only bother adding to the output if there are connected componenets present
                     components.append(str(len(tempvar)) +' '+  ' '.join([str(y) for y in tempvar]))
-        print(str(len(components))+'\n'+ '\n'.join(components))
+        print(str(len(components))+'\n'+ '\n'.join(components)) #formatting the output
         
-    
+    def minDistance(self, start):
+        visisted = []
+        
+                
     def DFSutl(self,i, visited):
-        visited[i] = True
-        val = []
+        visited[i] = True #We visit the node and color it
+        val = [] #for returning
         val.append(i)
-        for it in self.nodeList[i].head:
-            if (not visited[it]):
+        for it in self.nodeList[i].head: #iterate through the secondary structure
+            if (not visited[it]): #If we haven't seen it before, recurse
                val.extend(self.DFSutl(it, visited))
         return val
 
@@ -55,12 +63,13 @@ def read():
         for y in range(edgenum):
             temp = line.pop(0)
             graph.addEdge(i,temp)
-    return graph
+    
+    return graph, int(raw_input())
 
 def main():
     #Main function to call
-    g = read()
-    g.connected()
+    g, start = read()
+    
 
 if __name__ == "__main__":
     #If it is being called from the python interpreter, start main
